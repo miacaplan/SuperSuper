b54eb49dfd1ca39a3bf891f877473d2a727d084b
Udi Oron <udioron@gmail.com>
2016-06-26 12:09:02 +0300

    Add dependency injection for component template url.

--- store/static/store/cart.component.js
+++ store/static/store/cart.component.js
@@ -1,10 +1,10 @@
 angular.module("store").component('cart', {
-    templateUrl: "/static/store/cart.component.html",  // TODO: how to inject to component?
+    templateUrl: basePath => basePath + "cart.component.html",
     bindings: {
         items: '=',
     },
     controller: function CartController() {
-        this.total = () =>  this.items.reduce(
+        this.total = () => this.items.reduce(
             (prev, current) => prev + current.product.price * current.amount, 0)
     }
 });
